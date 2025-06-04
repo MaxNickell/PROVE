5da2d89660f1f463de2e622b572c425b89c7c604
Max Nickell <maxanickell@gmail.com>
2025-06-03 20:40:09 -0400

    blip device mapping

--- vision/blip_image_text_to_text.py
+++ vision/blip_image_text_to_text.py
@@ -1,16 +1,14 @@
 import requests
-import torch
 from PIL import Image
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 class BlipImageTextToText:
     def __init__(self, model_name: str = "Salesforce/blip2-flan-t5-xl") -> None:
         self.processor = Blip2Processor.from_pretrained(model_name, use_fast=True)
-        self.device = "cuda:0"
-        self.model = Blip2ForConditionalGeneration.from_pretrained(model_name, device_map={"": self.device})
+        self.model = Blip2ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
 
     def caption(self, image: Image.Image, query: str) -> str: 
-        inputs = self.processor(image, query, return_tensors="pt").to(self.device)
+        inputs = self.processor(image, query, return_tensors="pt").to("cuda")
         out = self.model.generate(**inputs)
         return self.processor.decode(out[0], skip_special_tokens=True)
 
